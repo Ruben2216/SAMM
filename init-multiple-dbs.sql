a85bc17b95ec17f8c5cc74564c7beefcdcf861a6
Ruben Clemente <jose_clemente_corzo@hotmail.com>
2026-05-04 19:38:38 -0600

    intento de despliegue 1

---
+++ init-multiple-dbs.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE samm_citas_db;
+CREATE DATABASE samm_medicamentos_db;
+CREATE DATABASE samm_tracking_db;
+CREATE DATABASE samm_notificaciones_db;
+-- La base de datos principal 'samm_db' se crea automáticamente con las variables de Docker.
